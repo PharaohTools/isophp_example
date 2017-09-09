6f6d4e5c2c490a6b33f052535be5c331db0187aa
David Amanshia <phpengine@pharaohtools.com>
2017-09-09 05:40:24 +0100

    iso docs mod

--- app/Docs/Model/DocsPageModel.php
+++ app/Docs/Model/DocsPageModel.php
@@ -14,13 +14,20 @@ class PageModel extends \Model\Base {
 
     public static function bindButtons() {
         return function () {
-            \ISOPHP\js_core::$console->log('Binding buttons') ;
-            $jQuery = \ISOPHP\js_core::$jQuery ;
-            $go_landing_page = $jQuery('.link_home') ;
-            $go_landing_page->on('click', function () {
-                $navigate = new \Model\Navigate() ;
-                $navigate->route('LandingPage', 'show', array(), '/') ;
-            }) ;
+            if (ISOPHP_EXECUTION_ENVIRONMENT === 'UNITER') {
+                \ISOPHP\js_core::$console->log('Binding buttons') ;
+                $jQuery = \ISOPHP\js_core::$jQuery ;
+                $go_landing_page = $jQuery('.link_home') ;
+                $go_landing_page->on('click', function () {
+                    $navigate = new \Model\Navigate() ;
+                    $navigate->route('LandingPage', 'show', array(), '/') ;
+                }) ;
+                $go_get_started = $jQuery('.link_getstarted') ;
+                $go_get_started->on('click', function () {
+                    $navigate = new \Model\Navigate() ;
+                    $navigate->route('GetStarted', 'show', array(), '/getstarted') ;
+                }) ;
+            }
         } ;
     }
 
--- app/Docs/View/DocsView.php
+++ app/Docs/View/DocsView.php
@@ -6,13 +6,12 @@ class DocsView extends \Core\View {
 
     public function display($data) {
 //        \ISOPHP\js_core::$console->log('data', $data ) ;
-        $jQuery =  \ISOPHP\js_core::$jQuery ;
         \ISOPHP\js_core::$window->document->title = 'Docs' ;
         $tplfunc = \Core\View::$template ;
-        $tpl_data = $tplfunc() ;
-//        \ISOPHP\js_core::$console->log('davey ravey don', $tpl_data) ;
-        $jQuery('#template')->html($tpl_data) ;
-        $jQuery('.app-loader')->css('display', 'none') ;
+        $tpl_data = \Core\View::parse_view_template($tplfunc) ;
+        \Core\View::execute_view_template('#template', $tpl_data) ;
+        \ISOPHP\js_core::$console->log('davey ravey don') ;
+        \ISOPHP\js_core::$console->log($tpl_data) ;
     }
 
 }
