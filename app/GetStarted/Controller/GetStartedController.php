3a5bbe56decb81c298aee2cfe15342592ee59de3
David Amanshia <phpengine@pharaohtools.com>
2017-09-20 22:00:41 +0100

    console through class

--- app/GetStarted/Controller/GetStartedController.php
+++ app/GetStarted/Controller/GetStartedController.php
@@ -7,7 +7,7 @@ class GetStartedController extends \Controller\Base {
     public function execute($pageVars) {
         $page_model = new \Model\LandingPage\PageModel() ;
         $page = $page_model->getPage() ;
-        \ISOPHP\js_core::$console->log('ICP', $page) ;
+        \ISOPHP\console::log('ICP', $page) ;
         \ISOPHP\js_core::$window->document->title = 'isophp - The Isomorphic PHP Framework' ;
         $res = new \Controller\Result() ;
         $res->page = $page ;
@@ -15,7 +15,7 @@ class GetStartedController extends \Controller\Base {
         $res->type = 'view' ;
         $res->view_control = 'GetStarted' ;
         $res->post_template[] = $page_model->bindButtons() ;
-        \ISOPHP\js_core::$console->log('LandingPage Con', $res) ;
+        \ISOPHP\console::log('LandingPage Con', $res) ;
         return $res ;
     }
 
