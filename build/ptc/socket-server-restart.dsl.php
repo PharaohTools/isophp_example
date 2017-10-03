7430855166041f4ccaacef74cf7fe911fd91cef4
David Amanshia <phpengine@pharaohtools.com>
2017-10-03 23:46:36 +0100

    socket server restart

--- build/ptc/socket-server-restart.dsl.php
+++ build/ptc/socket-server-restart.dsl.php
@@ -1,12 +1,12 @@
 Process kill
   label "Kill any running socketserve.js server"
-  name "socketserve.js"
+  name "node"
   use-pkill
   yes
 
 RunCommand execute
-  label "Start socket server bash -c 'node {{{ param::start-dir }}}/server/node_socket_server/socketserve.js &' & \n"
-  command "bash -c 'node {{{ param::start-dir }}}/server/node_socket_server/socketserve.js &' & \n"
+  label "Start socket server bash -c 'node {{{ param::start-dir }}}/server/node_socket_server/socketserve.js &' 2&>1 "
+  command "bash -c 'node {{{ param::start-dir }}}/server/node_socket_server/socketserve.js &' 2&>1 "
   nohup
   guess
 
