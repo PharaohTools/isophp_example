35de00922cdd4c26a9cf3ddf583813388ad0f265
David Amanshia <phpengine@pharaohtools.com>
2017-10-03 23:45:37 +0100

    socket server in vm assets

--- build/ptc/all-vm-web-assets.dsl.php
+++ build/ptc/all-vm-web-assets.dsl.php
@@ -1,9 +1,14 @@
 RunCommand execute
-  label "Build the Web Client Application"
-  command "cd {{{ param::start-dir }}}/ && ptconfigure auto x --af=build/ptc/build-web-client-application.dsl.php --start-dir=`pwd` --vars=vars/vm.php "
+  label "Restart the Node Socket Server"
+  command "cd {{{ param::start-dir }}} && ptconfigure auto x --af=build/ptc/socket-server-restart.dsl.php --start-dir=`pwd` --vars=vars/vm.php "
   guess
 
 RunCommand execute
-  label "Build the Web Server Application"
-  command "cd {{{ param::start-dir }}}/ && ptconfigure auto x --af=build/ptc/build-web-server-application.dsl.php --start-dir=`pwd` --vars=vars/vm.php "
+  label "Build the ISOPHP Web Client Application"
+  command "cd {{{ param::start-dir }}} && ptconfigure auto x --af=build/ptc/build-web-client-application.dsl.php --start-dir=`pwd` --vars=vars/vm.php "
+  guess
+
+RunCommand execute
+  label "Build the ISOPHP Web Server Application"
+  command "cd {{{ param::start-dir }}} && ptconfigure auto x --af=build/ptc/build-web-server-application.dsl.php --start-dir=`pwd` --vars=vars/vm.php "
   guess
