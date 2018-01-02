0a400de56699acb74f6393f2a3d6c0d7929b6a7e
David Amanshia <phpengine@pharaohtools.com>
2018-01-02 02:49:50 +0000

    require path fix

--- core/isophp.php
+++ core/isophp.php
@@ -16,7 +16,7 @@ class core {
         if (ISOPHP_EXECUTION_ENVIRONMENT === 'ZEND') {
             require (REQUIRE_PREFIX.DIRECTORY_SEPARATOR.'uniter_bundle'.DIRECTORY_SEPARATOR.'file_index.fephp') ;
         } else {
-            require (DIRECTORY_SEPARATOR.'uniter_bundle'.DIRECTORY_SEPARATOR.'file_index.fephp') ;
+            require (REQUIRE_PREFIX.DIRECTORY_SEPARATOR.'uniter_bundle'.DIRECTORY_SEPARATOR.'file_index.fephp') ;
         }
         return $file_index ;
     }
