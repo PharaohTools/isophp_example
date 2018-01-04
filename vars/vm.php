c1cb892f1ddee2ba9fb997b28845eab88bf1cc99
David Amanshia <phpengine@pharaohtools.com>
2018-01-04 11:52:46 +0000

    defaut var set

--- vars/vm.php
+++ vars/vm.php
@@ -5,12 +5,14 @@ $variables['vmname'] = $variables['application_slug'] ;
 $variables['domain'] = $variables['vmname'].'.vm' ;
 $variables['desktop_app_slug'] = $variables['vmname'] ;
 $variables['android_shell_script'] = 'vm-android-shell.bash' ;
+
+if (isset($params['backendenv'])) {
+    $variables['backendenv'] = $params['backendenv'] ;
+    if (in_array($params['backendenv'], array('local', 'devcloud'))) {
+        $variables['custom_branch'] = 'development' ;
+    }
+}
+
 # Developer build (Virtual Machine) can use a back end of either local (VM) or devcloud
-if (isset($params['mobilebackend'])) {
-    $variables['mobilebackend'] = $params['mobilebackend'] ;
-}
-else {
-    $variables['mobilebackend'] = 'local' ;
-}
 //var_dump('vm vars', __DIR__.DIRECTORY_SEPARATOR.'default.php', $variables) ;
 
