69550ad128e9c846246ad0e540fdae4844190ce7
David Amanshia <david.amanshia@sapient.com>
2018-02-23 09:48:45 +0000

    fixed model name

--- app/GetStarted/Controller/GetStartedController.php
+++ app/GetStarted/Controller/GetStartedController.php
@@ -5,7 +5,7 @@ Namespace Controller ;
 class GetStartedController extends \Controller\Base {
 
     public function execute($pageVars) {
-        $page_model = new \Model\LandingPage\PageModel() ;
+        $page_model = new \Model\GetStarted\PageModel() ;
         $page = $page_model->getPage() ;
         \ISOPHP\console::log('ICP', $page) ;
         \ISOPHP\js_core::$window->document->title = 'ISOPHP Getting Started - The Isomorphic PHP Framework' ;
