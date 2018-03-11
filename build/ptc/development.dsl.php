4b5ab139fb3529ecb0fd2155644254f3e57b7c86
David Amanshia <phpengine@pharaohtools.com>
2018-03-11 23:56:34 +0000

    dev settings

--- build/ptc/development.dsl.php
+++ build/ptc/development.dsl.php
@@ -1,107 +1,107 @@
-GitKeySafe ensure
-  guess
-
-Composer ensure
-  guess
-
-NodeJS install
-
-RunCommand execute
-  label "Install prequisite packages"
-  command "apt-get install -y apache2 libapache2-mod-php5 sqlite3 php5-sqlite zip unzip"
-  guess
-
-RunCommand execute
-  label "NPM Install Node Version 6"
-  command "npm cache clean -f && npm install -g n && n 6.0.0 && ln -sf /usr/local/n/versions/node/6.0.0/bin/node /usr/bin/nodejs"
-  guess
-
-RunCommand execute
-  label "Check if Electron Packager and cordova are installed"
-  command 'ISINSTEP=`which electron-packager > /dev/null && echo $?` && ISINSTC=`which cordova > /dev/null && echo $?` && BOTH_INST=`! (( $ISINSTEP | $ISINSTC )); echo $?` && if [ "$BOTH_INST" = "0" ] ; then echo "true" ; fi'
-  guess
-  ignore_errors
-  register "electron_cordova_are_installed"
-
-RunCommand execute
-  label "NPM Install Electron Packager and cordova"
-  command "npm install --silent -g electron-packager cordova browserify uglifyjs uglify-js cordova-icon > /dev/null"
-  guess
-  not_when "{{{ param::electron_cordova_are_installed }}}"
-  equals "true"
-
-Java install
-  version "1.8"
-  guess
-
-PTDeploy ensure
-  guess
-
-PTBuild ensure
-  guess
-  label "Lets ensure Pharaoh Build"
-  vhe-url "build.{{{ var::vmname }}}.vm"
-  vhe-ip-port "0.0.0.0:80"
-  version latest
-  guess
-
-RunCommand execute
-  label "Install SDKManager for Gradle"
-  command 'echo ptv | sudo -S su -c "(export SDKMAN_DIR=/home/ptbuild/.sdkman && curl -s https://get.sdkman.io | bash > /dev/null)" - ptbuild'
-  guess
-
-RunCommand execute
-  label "Install Gradle using SDKManager"
-  command "echo ptv | sudo -S su -c 'source /home/ptbuild/.sdkman/bin/sdkman-init.sh && sdk install gradle 4.0.2' - ptbuild"
-  guess
-
-RunCommand execute
-  label "Check if the Android SDK tools are installed"
-  command 'ISINST=`ls /home/ptbuild/Android/Sdk/tools/bin/sdkmanager` && if [ "$ISINST" = "/home/ptbuild/Android/Sdk/tools/bin/sdkmanager" ] ; then echo "true" ; fi '
-  guess
-  ignore_errors
-  register "android_sdk_tools_are_installed"
-
-RunCommand execute
-  label "Copy the host shared SDK tools"
-  command "cp /var/www/hostshare/build/binaries/sdk-tools-linux-3859397.zip /tmp/sdk-tools.zip"
-  guess
-  ignore_errors
-  not_when "{{{ param::android_sdk_tools_are_installed }}}"
-  equals "true"
-
-RunCommand execute
-  label "Get the Android SDK tools"
-  command "rm -f /tmp/sdk-tools.zip ] ; curl -o /tmp/sdk-tools.zip https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip ; "
-  guess
-  not_when "{{{ param::android_sdk_tools_are_installed }}}"
-  equals "true"
-
-RunCommand execute
-  label "Make SDK Dir - MUST RUN AFTER PTBUILD INSTALL OR USER WONT EXIST"
-  command "mkdir -p /home/ptbuild/Android/Sdk && chown -R ptbuild:ptbuild /home/ptbuild/Android/Sdk"
-  guess
-
-RunCommand execute
-  label "Unzip SDK Tools"
-  command "cd /tmp && mv sdk-tools.zip /home/ptbuild/Android/Sdk && cd /home/ptbuild/Android/Sdk && rm -rf /home/ptbuild/Android/Sdk/tools && unzip -qq sdk-tools.zip && chown -R ptbuild:ptbuild /home/ptbuild/Android"
-  guess
-  not_when "{{{ param::android_sdk_tools_are_installed }}}"
-  equals "true"
-
-RunCommand execute
-  label "Install the android build tools with android sdkmanager"
-  command "source /etc/profile && source /var/www/hostshare/build/vm-android-shell.bash && echo y | sdkmanager 'build-tools;26.0.0'"
-  guess
-  ignore_errors
-
-SudoNoPass install
-  label "Sudo ability for Pharaoh Build user"
-  install-user-name ptbuild
-
-Logging log
-  log-message "Lets copy in our build pipes"
-  source Autopilot
+#GitKeySafe ensure
+#  guess
+#
+#Composer ensure
+#  guess
+#
+#NodeJS install
+#
+#RunCommand execute
+#  label "Install prequisite packages"
+#  command "apt-get install -y apache2 libapache2-mod-php5 sqlite3 php5-sqlite zip unzip"
+#  guess
+#
+#RunCommand execute
+#  label "NPM Install Node Version 6"
+#  command "npm cache clean -f && npm install -g n && n 6.0.0 && ln -sf /usr/local/n/versions/node/6.0.0/bin/node /usr/bin/nodejs"
+#  guess
+#
+#RunCommand execute
+#  label "Check if Electron Packager and cordova are installed"
+#  command 'ISINSTEP=`which electron-packager > /dev/null && echo $?` && ISINSTC=`which cordova > /dev/null && echo $?` && BOTH_INST=`! (( $ISINSTEP | $ISINSTC )); echo $?` && if [ "$BOTH_INST" = "0" ] ; then echo "true" ; fi'
+#  guess
+#  ignore_errors
+#  register "electron_cordova_are_installed"
+#
+#RunCommand execute
+#  label "NPM Install Electron Packager and cordova"
+#  command "npm install --silent -g electron-packager cordova browserify uglifyjs uglify-js cordova-icon > /dev/null"
+#  guess
+#  not_when "{{{ param::electron_cordova_are_installed }}}"
+#  equals "true"
+#
+#Java install
+#  version "1.8"
+#  guess
+#
+#PTDeploy ensure
+#  guess
+#
+#PTBuild ensure
+#  guess
+#  label "Lets ensure Pharaoh Build"
+#  vhe-url "build.{{{ var::vmname }}}.vm"
+#  vhe-ip-port "0.0.0.0:80"
+#  version latest
+#  guess
+#
+#RunCommand execute
+#  label "Install SDKManager for Gradle"
+#  command 'echo ptv | sudo -S su -c "(export SDKMAN_DIR=/home/ptbuild/.sdkman && curl -s https://get.sdkman.io | bash > /dev/null)" - ptbuild'
+#  guess
+#
+#RunCommand execute
+#  label "Install Gradle using SDKManager"
+#  command "echo ptv | sudo -S su -c 'source /home/ptbuild/.sdkman/bin/sdkman-init.sh && sdk install gradle 4.0.2' - ptbuild"
+#  guess
+#
+#RunCommand execute
+#  label "Check if the Android SDK tools are installed"
+#  command 'ISINST=`ls /home/ptbuild/Android/Sdk/tools/bin/sdkmanager` && if [ "$ISINST" = "/home/ptbuild/Android/Sdk/tools/bin/sdkmanager" ] ; then echo "true" ; fi '
+#  guess
+#  ignore_errors
+#  register "android_sdk_tools_are_installed"
+#
+#RunCommand execute
+#  label "Copy the host shared SDK tools"
+#  command "cp /var/www/hostshare/build/binaries/sdk-tools-linux-3859397.zip /tmp/sdk-tools.zip"
+#  guess
+#  ignore_errors
+#  not_when "{{{ param::android_sdk_tools_are_installed }}}"
+#  equals "true"
+#
+#RunCommand execute
+#  label "Get the Android SDK tools"
+#  command "rm -f /tmp/sdk-tools.zip ] ; curl -o /tmp/sdk-tools.zip https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip ; "
+#  guess
+#  not_when "{{{ param::android_sdk_tools_are_installed }}}"
+#  equals "true"
+#
+#RunCommand execute
+#  label "Make SDK Dir - MUST RUN AFTER PTBUILD INSTALL OR USER WONT EXIST"
+#  command "mkdir -p /home/ptbuild/Android/Sdk && chown -R ptbuild:ptbuild /home/ptbuild/Android/Sdk"
+#  guess
+#
+#RunCommand execute
+#  label "Unzip SDK Tools"
+#  command "cd /tmp && mv sdk-tools.zip /home/ptbuild/Android/Sdk && cd /home/ptbuild/Android/Sdk && rm -rf /home/ptbuild/Android/Sdk/tools && unzip -qq sdk-tools.zip && chown -R ptbuild:ptbuild /home/ptbuild/Android"
+#  guess
+#  not_when "{{{ param::android_sdk_tools_are_installed }}}"
+#  equals "true"
+#
+#RunCommand execute
+#  label "Install the android build tools with android sdkmanager"
+#  command "source /etc/profile && source /var/www/hostshare/build/vm-android-shell.bash && echo y | sdkmanager 'build-tools;26.0.0'"
+#  guess
+#  ignore_errors
+#
+#SudoNoPass install
+#  label "Sudo ability for Pharaoh Build user"
+#  install-user-name ptbuild
+#
+#Logging log
+#  log-message "Lets copy in our build pipes"
+#  source Autopilot
 
 RunCommand execute
   label "Get the names of the Build pipes"
@@ -109,11 +109,15 @@ RunCommand execute
   guess
   register "build_pipe_names"
 
+Logging log
+  log-message "build pipe names are {{{ param::build_pipe_names }}}"
+  source Autopilot
+
 RunCommand execute
   label "Import the Development Build pipes"
   command "ptbuild importexport import -yg --source=/var/www/hostshare/build/ptbuild/pipes/{{ loop }}"
-  guess
   loop "{{{ param::build_pipe_names }}}"
+  guess
 
 RunCommand execute
   label "Create a default admin user"
