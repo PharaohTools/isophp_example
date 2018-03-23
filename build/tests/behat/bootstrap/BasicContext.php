97400b1821ddfd258fc942484c004023946348b9
David Amanshia <david.amanshia@sapient.com>
2018-03-23 22:44:35 +0000

    A bit more basic behat, its not ready yet but a save

--- build/tests/behat/bootstrap/BasicContext.php
+++ build/tests/behat/bootstrap/BasicContext.php
@@ -11,11 +11,11 @@ use Behat\MinkExtension\Context\MinkContext;
  * Behat context class.
  */
 
-class NoActionsContext implements Context {
+class BasicContext implements Context {
 
 
     /**
-     * @Given /^I run the application command in the shell$/
+     * @Given /^I go to the start page$/
      */
     public function iRunTheApplicationCommandInTheShell()
     {
@@ -23,62 +23,4 @@ class NoActionsContext implements Context {
         exec($command, $output);
         $this->output = trim(implode("\n", $output));
     }
-
-    /**
-     * @Then /^I should see all of the modules which are not hidden$/
-     */
-    public function iShouldSeeAllOfTheModulesWhichAreNotHidden()
-    {
-        $mi = \Core\AutoLoader::getInfoObjects() ;
-        foreach ($mi as $moduleInfo) {
-            if ($moduleInfo->hidden != true) {
-                $array_keys = array_keys($moduleInfo->routesAvailable()) ;
-                $command = $array_keys[0] ;
-                if (strpos($this->output, $command) === false) {
-                    throw new \Exception("Expected module {$command} not found."); } } }
-    }
-
-    /**
-     * @Then /^I should see the application description$/
-     */
-    public function iShouldSeeTheApplicationDescription()
-    {
-
-    }
-
-    /**
-     * @Then /^I should see the cli text "([^"]*)"$/
-     */
-    public function iShouldSeeTheCliText($arg1)
-    {
-        if (strpos($this->output, $arg1) === false) {
-            throw new \Exception("Expected text $arg1 not found."); }
-    }
-
-    /**
-     * @Given /^I run the application command in the shell with parameter string "([^"]*)"$/
-     */
-    public function iRunTheApplicationCommandInTheShellWithParameterString($str)
-    {
-        $command = PTCCOMM." $str" ;
-        exec($command, $output);
-        $this->output = trim(implode("\n", $output));
-    }
-
-    /**
-     * @Then /^I should see only the modules which are compatible with this system$/
-     */
-    public function iShouldSeeOnlyTheModulesWhichAreCompatibleWithThisSystem()
-    {
-        //@todo this functionality wont be 110 accurate without unit testing that method it calls from model
-        $iao = new \Model\IndexAllOS(array()) ;
-        $method = new ReflectionMethod('\Model\IndexAllOS', 'findOnlyCompatibleModuleNames');
-        $method->setAccessible(true);
-        $compats = $method->invokeArgs($iao,array(array()));
-        foreach ($compats as $compat) {
-            if ($compat["hidden"] !== true) {
-                if (strpos($this->output, $compat["command"]) === false) {
-                    throw new \Exception("Expected compatible module {$compat["command"]} not found."); } } }
-    }
-
 }
--- build/tests/behat/features/ExampleHome.feature
+++ build/tests/behat/features/ExampleHome.feature
@@ -1,25 +1,26 @@
 @home
-Feature: Executing the program index page
-  As a command line user
-  I want to execute the index page
+Feature: Executing the Application
+  As an interface user
+  I want to execute the home page
   To see overview information about the application
 
-  Scenario: Execute with no parameters
+  Scenario: See the Logo
+    Given I go to the start page
     Given I run the application command in the shell
     Then I should see all of the modules which are not hidden
 
-  Scenario: Execute with no parameters
-    Given I run the application command in the shell
-    Then I should see the application description
-
-  Scenario: Execute with no parameters
-    Given I run the application command in the shell
-    Then I should see the cli text "www.pharaohtools.com"
-
-  Scenario: Execute with "--only-compatible" parameter
-    Given I run the application command in the shell with parameter string "--only-compatible"
-    Then I should see only the modules which are compatible with this system
-
-  Scenario: Execute with "--compatible-only" parameter
-    Given I run the application command in the shell with parameter string "--compatible-only"
-    Then I should see only the modules which are compatible with this system
+#  Scenario: Execute with no parameters
+#    Given I run the application command in the shell
+#    Then I should see the application description
+#
+#  Scenario: Execute with no parameters
+#    Given I run the application command in the shell
+#    Then I should see the cli text "www.pharaohtools.com"
+#
+#  Scenario: Execute with "--only-compatible" parameter
+#    Given I run the application command in the shell with parameter string "--only-compatible"
+#    Then I should see only the modules which are compatible with this system
+#
+#  Scenario: Execute with "--compatible-only" parameter
+#    Given I run the application command in the shell with parameter string "--compatible-only"
+#    Then I should see only the modules which are compatible with this system
