aeb08627bcb0b6ced81e5f0b4cc5890bee82e6e4
David Amanshia <david.amanshia@sapient.com>
2018-03-24 00:03:14 +0000

    first draft of vm to artefact repo

--- build/ptc/push-vm-to-cloud.dsl.php
+++ build/ptc/push-vm-to-cloud.dsl.php
@@ -1,14 +1,9 @@
 RunCommand execute
-  label "Install Software"
-  command "cd {{{ param::start-dir }}}/ && ptconfigure auto x --af=build/ptc/development.dsl.php --start-dir=`pwd` --vars=vars/vm.php "
+  label "Create latest ISO PHP PTV Image"
+  command "cd {{{ param::start-dir }}}/ && ptv auto x ptvirtualize box package --name='ISO PHP Example App' --vmname='iso_php_example_application' --group=ptvirtualize --description='$$description' -yg "
   guess
 
 RunCommand execute
-  label "Build the Web Client Application"
-  command "cd {{{ param::start-dir }}}/ && ptconfigure auto x --af=build/ptc/build-web-client-application.dsl.php --start-dir=`pwd` --vars=vars/vm.php "
-  guess
-
-RunCommand execute
-  label "Build the Web Server Application"
-  command "cd {{{ param::start-dir }}}/ && ptconfigure auto x --af=build/ptc/build-web-server-application.dsl.php --start-dir=`pwd` --vars=vars/vm.php "
+  label "Push it to Pharaoh Repos"
+  command "curl -F file=@/opt/p  -F control=BinaryServer -F action=serve -F item=iso_php_virtualize_boxes -F auth_user=$$pharaoh_repo_auth_user -F auth_pw=$$pharaoh_repo_auth_pw $$pharaoh_repo_url --start-dir=`pwd` --vars=vars/vm.php "
   guess
