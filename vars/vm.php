05d13c3b2558c053f5dd9701479880104e2a1e92
David Amanshia <david.amanshia@sapient.com>
2018-03-24 08:04:08 +0000

    new default vars for vm. Pushing vm images to artefact repo vars

--- vars/vm.php
+++ vars/vm.php
@@ -7,6 +7,7 @@ $variables['desktop_app_slug'] = $variables['vmname'] ;
 $variables['android_shell_script'] = 'vm-android-shell.bash' ;
 $variables['custom_branch'] = 'development' ;
 $variables['env_level'] = 'dev' ;
+$variables['description_vm_image'] = 'The PTV VM Image for '.$variables['description'] ;
 
 if (isset($params['backendenv'])) {
     $variables['backendenv'] = $params['backendenv'] ;
@@ -17,6 +18,10 @@ if (isset($params['backendenv'])) {
     $params['backendenv'] = 'local' ;
     $variables['backendenv'] = $params['backendenv'] ;
 }
+
+$variables['pharaoh_repo_auth_user'] = '' ;
+$variables['pharaoh_repo_auth_pw'] = '' ;
+$variables['pharaoh_repo_auth_url'] = '' ;
 
 # Developer build (Virtual Machine) can use a back end of either local (VM) or devcloud
 //var_dump('vm vars', __DIR__.DIRECTORY_SEPARATOR.'default.php', $variables) ;
