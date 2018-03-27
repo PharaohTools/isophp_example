edaccffe179c986d2355483255dc3e6773d40486
David Amanshia <david.amanshia@sapient.com>
2018-03-27 12:26:38 +0100

    basic funtionality updated

--- build/tests/behat/bootstrap/FeatureContext.php
+++ build/tests/behat/bootstrap/FeatureContext.php
@@ -14,8 +14,7 @@ use Behat\Testwork\Hook\Scope\AfterSuiteScope;
  * Behat context class.
  */
 
-class FeatureContext  implements Context
-{
+class FeatureContext  implements Context {
 
     private $output;
     public static $session ;
@@ -31,6 +30,81 @@ class FeatureContext  implements Context
 
     private function setup() {
 
+    }
+
+    /**
+     * @Given /^I go to the start page$/
+     */
+    public function iGoToTheStartPage()
+    {
+// Choose a Mink driver. More about it in later chapters.
+//        $driver = new \Behat\Mink\Driver\Selenium2Driver();
+        $driver = new \Behat\Mink\Driver\GoutteDriver();
+        $session = new \Behat\Mink\Session($driver);
+        \FeatureContext::$session = $session ;
+        $session->start();
+        $session->visit('http://www.isophpexampleapplication.vm');
+    }
+
+    /**
+     * @Given I go to the page :arg1
+     */
+    public function iGoToThePage($arg1)
+    {
+// Choose a Mink driver. More about it in later chapters.
+//        $driver = new \Behat\Mink\Driver\Selenium2Driver();
+        $driver = new \Behat\Mink\Driver\GoutteDriver();
+        $session = new \Behat\Mink\Session($driver);
+        \FeatureContext::$session = $session ;
+        $session->start();
+        $session->visit('http://www.isophpexampleapplication.vm'.$arg1);
+    }
+
+    /**
+     * @Given /^I find the logo$/
+     */
+    public function iFindTheLogo()
+    {
+        $session = \FeatureContext::$session ;
+        $page = $session->getPage();
+        $logo = $page->find('css', '.header_logo');
+        if ($logo == null) {
+            throw new \Exception('Unable to find logo');
+        }
+    }
+
+    /**
+     * @Given /^I have a correct status code$/
+     */
+    public function iHaveACorrectStatusCode()
+    {
+        $session = \FeatureContext::$session ;
+        $code = $session->getStatusCode() ;
+        if ($code !== '200') {
+            throw new \Exception('Status code not 200');
+        }
+    }
+
+
+    /**
+     * @Then I should see :arg1
+     */
+    public function iShouldSee2($arg1)
+    {
+        $session = \FeatureContext::$session ;
+        $page = $session->getPage();
+        $text = $page->getText();
+        if (stripos($text, $arg1) == false) {
+            var_dump($text) ;
+            throw new \Exception('Unable to see "'.$arg1.'"');
+        }
+    }
+
+    /**
+     * @Given I go to the home page
+     */
+    public function iGoToTheHomePage(){
+        $this->iGoToTheStartPage() ;
     }
 
 }
