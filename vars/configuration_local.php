df9f24c59e0cbb98400404ae4b9e13e607ec75b7
David Amanshia <phpengine@pharaohtools.com>
2018-07-14 23:26:39 +0100

    uniter build level based file extensions

--- vars/configuration_local.php
+++ vars/configuration_local.php
@@ -7,7 +7,19 @@ class Configuration {
     public static $config ;
 
     public static function variable($var) {
-        require (REQUIRE_PREFIX.'/core/default.fephp') ;
+        $file_ext = 'php' ;
+        if (ISOPHP_EXECUTION_ENVIRONMENT === 'ZEND') {
+            $file_ext = 'php' ;
+        }
+        if (ISOPHP_EXECUTION_ENVIRONMENT === 'UNITER') {
+            if (UNITER_BUILD_LEVEL == 'production') {
+                $file_ext = 'php' ;
+            }
+            if (UNITER_BUILD_LEVEL !== 'production') {
+                $file_ext = 'fephp' ;
+            }
+        }
+        require (REQUIRE_PREFIX.'/core/default.'.$file_ext) ;
         $config = \Model\Configuration::$config ;
         $config['ISOPHP_API_SERVER_URL'] = 'http://server.'.$config['application_slug'].'.vm:80'.$config['random_port_suffix'] ;
         $config['env_level'] = 'dev' ;
