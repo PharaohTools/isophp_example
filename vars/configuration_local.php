707226a32e659d5fd8dd722e112be7a901bc3f6a
David Amanshia <phpengine@pharaohtools.com>
2018-07-15 12:49:47 +0100

    uniter build level fix

--- vars/configuration_local.php
+++ vars/configuration_local.php
@@ -7,17 +7,10 @@ class Configuration {
     public static $config ;
 
     public static function variable($var) {
-        $file_ext = 'php' ;
-        if (ISOPHP_EXECUTION_ENVIRONMENT === 'ZEND') {
+        if (UNITER_BUILD_LEVEL == 'production') {
             $file_ext = 'php' ;
-        }
-        if (ISOPHP_EXECUTION_ENVIRONMENT === 'UNITER') {
-            if (UNITER_BUILD_LEVEL == 'production') {
-                $file_ext = 'php' ;
-            }
-            if (UNITER_BUILD_LEVEL !== 'production') {
-                $file_ext = 'fephp' ;
-            }
+        } else {
+            $file_ext = 'fephp' ;
         }
         require (REQUIRE_PREFIX.'/core/default.'.$file_ext) ;
         $config = \Model\Configuration::$config ;
