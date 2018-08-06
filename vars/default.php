08e632698abbffa9861f014e4693eed9b1ccdcc9
David Amanshia <phpengine@pharaohtools.com>
2018-08-06 21:38:03 +0100

    including the backend env var

--- vars/default.php
+++ vars/default.php
@@ -9,3 +9,13 @@ $variables['server_subdomain'] = 'server' ;
 $variables['domain'] = $variables['application_slug'].'.vm' ;
 $variables['friendly_app_slug'] = 'ISOPHP' ;
 $variables['desktop_app_slug'] = $variables['friendly_app_slug'] ;
+
+if (isset($params['backendenv'])) {
+    $variables['backendenv'] = $params['backendenv'] ;
+    if (in_array($params['backendenv'], array('local', 'devcloud'))) {
+        $variables['custom_branch'] = 'development' ;
+    }
+} else {
+    $params['backendenv'] = 'local' ;
+    $variables['backendenv'] = $params['backendenv'] ;
+}
