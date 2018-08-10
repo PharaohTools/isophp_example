2047cbb2744013018f39fdfd3f9684a4e94fb540
David Amanshia <phpengine@pharaohtools.com>
2018-08-10 02:14:45 +0100

    populate confguration

--- vars/default.php
+++ vars/default.php
@@ -19,3 +19,18 @@ if (isset($params['backendenv'])) {
     $params['backendenv'] = 'local' ;
     $variables['backendenv'] = $params['backendenv'] ;
 }
+
+if (ISOPHP_EXECUTION_ENVIRONMENT == 'UNITER') {
+    \ISOPHP\js_core::$console->log('before loop') ;
+    \ISOPHP\js_core::$console->log($variables) ;
+    $temp_config = \Model\Configuration::$config ;
+    foreach ($variables as $this_key => $this_value) {
+        \ISOPHP\js_core::$console->log('this key is ' . $this_key) ;
+        \ISOPHP\js_core::$console->log('this value is ' . $this_value) ;
+        \ISOPHP\js_core::$console->log('config is ', $temp_config) ;
+        $temp_config[$this_key] = $this_value ;
+    }
+    \Model\Configuration::$config = $temp_config ;
+    \ISOPHP\js_core::$console->log('registry dump') ;
+    \ISOPHP\js_core::$console->log(\Model\Configuration::$config) ;
+}
