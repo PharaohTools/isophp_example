b28fb848467550133decd34f2d2fd5d8dcf426a4
David Amanshia <phpengine@pharaohtools.com>
2018-09-02 21:24:56 +0100

    restore controller

--- app/LandingPage/Controller/LandingPageController.php
+++ app/LandingPage/Controller/LandingPageController.php
@@ -5,17 +5,18 @@ Namespace Controller ;
 class LandingPageController extends \Controller\Base {
 
     public function execute($pageVars) {
+        \ISOPHP\console::log('LandingPageController Execute Method', $pageVars) ;
         $page_model = new \Model\LandingPage\PageModel() ;
         $page = $page_model->getPage() ;
-        \ISOPHP\console::log('ICP', $page) ;
-//        \ISOPHP\js_core::$window->document->title = 'ISOPHP Home - The Isomorphic PHP Framework' ;
         $res = new \Controller\Result() ;
         $res->page = $page ;
         $res->view = 'LandingPage.phptpl' ;
         $res->type = 'view' ;
         $res->view_control = 'LandingPage' ;
-        $res->post_template[] = $page_model->bindButtons() ;
-        $res->post_template[] = $page_model->bindChatApplication() ;
+        $post_template = $res->post_template ;
+        $post_template[] = $page_model->bindButtons() ;
+        $post_template[] = $page_model->bindChatApplication() ;
+        $res->post_template = $post_template ;
         \ISOPHP\console::log('LandingPage Con', $res) ;
         return $res ;
     }
