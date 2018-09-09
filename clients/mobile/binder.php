4e3f671ad6072c2ada59f777722885f481c1e391
David Amanshia <phpengine@pharaohtools.com>
2018-09-09 17:13:24 +0100

    using new php extension for view files, so only production settings work currently, and start of mobile production settings integration

---
+++ clients/mobile/binder.php
@@ -0,0 +1,19 @@
+<?php
+
+return function ($jQuery, $window, $console, $php, $file_index) {
+    $console->log('Binder Loaded!') ;
+    require("./core/constants_uniter_prod.php") ;
+    $console->log( "Require Prefix value in Binder: " . REQUIRE_PREFIX) ;
+    $console->log("Uniter Build Level in Binder: " . UNITER_BUILD_LEVEL)  ;
+    require(REQUIRE_PREFIX."/core/app_vars.php") ;
+    require(REQUIRE_PREFIX."/core/isophp.php") ;
+    \ISOPHP\js_core::$jQuery = $jQuery ;
+    \ISOPHP\js_core::$window = $window ;
+    \ISOPHP\js_core::$console = $console ;
+    \ISOPHP\core::$php = $php ;
+    \ISOPHP\core::$file_index = $file_index ;
+    require(REQUIRE_PREFIX."/core/init.php") ;
+    require(REQUIRE_PREFIX."/core/WindowMessage.php") ;
+    require(REQUIRE_PREFIX."/core/bootstrap.php") ;
+    require(REQUIRE_PREFIX."/core/index.php") ;
+};
---
+++ clients/mobile/prod_app_webpack.js
@@ -0,0 +1,33 @@
+/*
+ * Demo of UI interaction with jQuery+Uniter
+ *
+ * MIT license.
+ */
+'use strict';
+
+console.log('In prod app webpack js: 1a') ;
+
+var jQuery = require('jquery'),
+    php = require('phpjs'),
+    file_index = require('./uniter_bundle/file_index.js') ;
+
+var this_console = console ;
+var this_window = window ;
+
+const binderModule = require('./binder.php')();
+//
+// Hook stdout and stderr up to the console
+binderModule.getStdout().on('data', function (data) {
+    // console.info(data);
+    document.body.insertAdjacentHTML('beforeEnd', data);
+});
+binderModule.getStderr().on('data', function (data) {
+    console.warn(data);
+});
+
+// Run the PHP module, capture its result Closure
+// and cast it to a JavaScript function we can call
+const binder = binderModule.execute().getNative();
+
+// Call the PHP closure function returned from binder.php, from JavaScript!
+binder(jQuery, this_window, this_console, php, file_index);
