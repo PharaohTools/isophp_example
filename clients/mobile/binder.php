a3f853a95a6666e1f181a0eb6e81ea8cea79d4cb
David Amanshia <phpengine@pharaohtools.com>
2018-09-10 12:12:37 +0100

    prod mobile settings

--- clients/mobile/binder.php
+++ clients/mobile/binder.php
@@ -2,7 +2,7 @@
 
 return function ($jQuery, $window, $console, $php, $file_index) {
     $console->log('Binder Loaded!') ;
-    require("./core/constants_uniter_prod.php") ;
+    require("./www/core/constants_uniter_prod.php") ;
     $console->log( "Require Prefix value in Binder: " . REQUIRE_PREFIX) ;
     $console->log("Uniter Build Level in Binder: " . UNITER_BUILD_LEVEL)  ;
     require(REQUIRE_PREFIX."/core/app_vars.php") ;
