13bff90aa962b459fa8efde8b960c5d741da7b2e
David Amanshia <phpengine@pharaohtools.com>
2018-09-10 12:13:06 +0100

    prod settings fs

--- clients/mobile/fs.js
+++ clients/mobile/fs.js
@@ -3,71 +3,123 @@
  *
  * MIT license.
  */
-'use strict';
+    'use strict';
+
+console.log("Filesystem building") ;
+var fstype = process.argv[2] ;
+if (fstype === 'production') {
+    var extension = 'php' ;
+} else {
+    var extension = 'fephp' ;
+}
 
 var fs = require('fs'),
     globby = require('globby'),
     path = require('path'),
     files = globby.sync([
-        __dirname + '/www/app/*.fephp',
-        __dirname + '/www/app/*/*.fephp',
-        __dirname + '/www/app/*/*/*.fephp',
-        __dirname + '/www/app/*/*/*.fephp',
-        __dirname + '/www/app/*/View/*.fephp',
         __dirname + '/www/app/*/View/desktop/*.phptpl',
         __dirname + '/www/app/*/View/mobile/*.phptpl',
         __dirname + '/www/app/*/View/web/*.phptpl',
         __dirname + '/www/app/*/View/*.phptpl',
-        __dirname + '/www/core/*.fephp',
-        __dirname + '/www/core/*/*.fephp',
-        __dirname + '/www/core/*/*/*.fephp',
-        __dirname + '/www/core/*/*/*/*.fephp'
+        __dirname + '/www/app/*/View/desktop/*.php',
+        __dirname + '/www/app/*/View/mobile/*.php',
+        __dirname + '/www/app/*/View/web/*.php',
+        __dirname + '/www/app/*/View/*.php',
+        __dirname + '/www/app/*.' + extension ,
+        __dirname + '/www/app/*/*.' + extension ,
+        __dirname + '/www/app/*/*/*.' + extension ,
+        __dirname + '/www/app/*/*/*.' + extension ,
+        __dirname + '/www/app/*/View/*.' + extension ,
+        __dirname + '/www/core/*.' + extension ,
+        __dirname + '/www/core/*/*.' + extension ,
+        __dirname + '/www/core/*/*/*.' + extension ,
+        __dirname + '/www/core/*/*/*/*.' + extension
+    ]),
+    app_files = globby.sync([
+        __dirname + '/www/app/*/View/desktop/*.phptpl',
+        __dirname + '/www/app/*/View/mobile/*.phptpl',
+        __dirname + '/www/app/*/View/web/*.phptpl',
+        __dirname + '/www/app/*/View/*.phptpl',
+        __dirname + '/www/app/*/View/desktop/*.php',
+        __dirname + '/www/app/*/View/mobile/*.php',
+        __dirname + '/www/app/*/View/web/*.php',
+        __dirname + '/www/app/*/View/*.php',
+        __dirname + '/www/app/*.' + extension ,
+        __dirname + '/www/app/*/*.' + extension ,
+        __dirname + '/www/app/*/*/*.' + extension ,
+        __dirname + '/www/app/*/*/*.' + extension ,
+        __dirname + '/www/app/*/View/*.' + extension
+    ]),
+    core_files = globby.sync([
+        __dirname + '/www/core/*.' + extension ,
+        __dirname + '/www/core/*/*.' + extension ,
+        __dirname + '/www/core/*/*/*.' + extension ,
+        __dirname + '/www/core/*/*/*/*.' + extension
     ]),
     file_index = [],
     file_data = {},
+    core_file_index = [],
+    core_file_data = {},
+    app_file_index = [],
+    app_file_data = {},
     root = __dirname + '/' ;
 
 console.log("filesData\n") ;
 files.forEach(function (filePath) {
-    // fileData[path.relative(root, filePath)] = fs.readFileSync(filePath).toString();
     var short_path = filePath.replace(root, '') ;
-    short_path = short_path.replace('www/', '') ;
     console.log(short_path) ;
-    var one_file = fs.readFileSync(filePath).toString() ;
-    // console.log("File for: " + short_path + one_file) ;
-    file_data[short_path] = one_file ;
-    // console.log('fd', short_path, one_file) ;
+    file_data[short_path] = fs.readFileSync(filePath).toString() ;
     file_index.push(short_path) ;
 });
+core_files.forEach(function (filePath) {
+    var short_path = filePath.replace(root, '') ;
+    console.log(short_path) ;
+    core_file_data[short_path] = fs.readFileSync(filePath).toString() ;
+    core_file_index.push(short_path) ;
+});
+app_files.forEach(function (filePath) {
+    var short_path = filePath.replace(root, '') ;
+    console.log(short_path) ;
+    app_file_data[short_path] = fs.readFileSync(filePath).toString() ;
+    app_file_index.push(short_path) ;
+});
 
-// For mobile, need ot add the file index to the file data
-// file_data['uniter_bundle/file_index.fephp'] = '<?php\n\n$file_index = ' + JSON.stringify(file_index) + ' ;\n\n\\ISOPHP\\core::$file_index = $file_index ;' ;
 
-console.log("\n\nfile_data\n", file_data, JSON.stringify(file_data)) ;
+console.log("\n\napp_data\n", JSON.stringify(app_file_data)) ;
+fs.writeFileSync(
+    __dirname + '/www/uniter_bundle/app_file_data.js',
+    'module.exports = ' + JSON.stringify(app_file_data) + ';'
+);
+console.log("\n\ncore_data\n", JSON.stringify(core_file_data)) ;
+fs.writeFileSync(
+    __dirname + '/www/uniter_bundle/core_file_data.js',
+    'module.exports = ' + JSON.stringify(core_file_data) + ';'
+);
+console.log("\n\nfile_data\n", JSON.stringify(file_data)) ;
 fs.writeFileSync(
     __dirname + '/www/uniter_bundle/file_data.js',
     'module.exports = ' + JSON.stringify(file_data) + ';'
 );
-
 console.log("\n\nfile_index\n", JSON.stringify(file_index)) ;
 fs.writeFileSync(
     __dirname + '/www/uniter_bundle/file_index.js',
     'module.exports = ' + JSON.stringify(file_index) + ' ;'
 );
 
+
 // File index
-
 console.log("\n\nfile_index\n", JSON.stringify(file_index)) ;
-
 var file_string = '' ;
 file_string += '<?php\n\n$file_index = array(\n' ;
-
 file_index.forEach(function (one_short_path) {
     file_string += '\t"' + one_short_path + '",\n' ;
 }) ;
 file_string += '\n) ;' ;
 file_string += '\n\n\\ISOPHP\\core::$file_index = $file_index ;\n' ;
-
+fs.writeFileSync(
+    __dirname + '/www/uniter_bundle/file_index.fephp',
+    file_string
+);
 fs.writeFileSync(
     __dirname + '/www/uniter_bundle/file_index.php',
     file_string
