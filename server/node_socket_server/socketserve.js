754d27eb15102983c3eb541f542149852bb6b436
David Amanshia <phpengine@pharaohtools.com>
2018-09-14 16:02:10 +0100

    cors headers

--- server/node_socket_server/socketserve.js
+++ server/node_socket_server/socketserve.js
@@ -1,4 +1,11 @@
 var app = require('express')() ;
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 var http = require('http').Server(app) ;
 var io = require('socket.io')(http) ;
 
