58e274eefc539243ae92731941d2b738fb563f2a
David Amanshia <phpengine@pharaohtools.com>
2018-10-14 13:44:56 +0100

    electron remote set further up

--- clients/desktop/binder.php
+++ clients/desktop/binder.php
@@ -1,11 +1,12 @@
 <?php
 
-return function ($jQuery, $window, $console, $php, $file_index, $electron_remote) {
+return function ($jQuery, $window, $console, $php, $file_index, $electron) {
     $console->log('Binder Loaded!') ;
     require("./core/constants_uniter_prod.php") ;
     $console->log( "Require Prefix value in Binder: " . REQUIRE_PREFIX) ;
     $console->log("Uniter Build Level in Binder: " . UNITER_BUILD_LEVEL)  ;
     $console->log("window: ", $window)  ;
+    $console->log("electron: ", $electron)  ;
     require(REQUIRE_PREFIX."/core/app_vars.php") ;
     require(REQUIRE_PREFIX."/core/isophp.php") ;
     require(REQUIRE_PREFIX."/core/electron.php") ;
@@ -14,7 +15,6 @@ return function ($jQuery, $window, $console, $php, $file_index, $electron_remote
     \ISOPHP\js_core::$console = $console ;
     \ISOPHP\core::$php = $php ;
     \ISOPHP\core::$file_index = $file_index ;
-    \ISOPHP\electron::$BrowserWindow = $electron_remote ;
     require(REQUIRE_PREFIX."/core/init.php") ;
     require(REQUIRE_PREFIX."/core/WindowMessage.php") ;
     require(REQUIRE_PREFIX."/core/bootstrap.php") ;
